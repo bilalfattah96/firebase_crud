51ddc838f6c61ddc798197681d3d697a06b10693
bilal <bilal@yahoo.com>
2024-09-11 19:07:43 +0500

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+void main() async {
+ await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+);
+runApp(Crud());
+}
+
+class Crud extends StatelessWidget {
+  const Crud({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
